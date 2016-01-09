5596e06162fa9f1255ad2cb0c41886171f3512db
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-01-10 00:12:59 +0100

    - 0.3.0 - 2016-01-09 - The function extended, help updated

--- Export-ADUserPhoto.ps1
+++ Export-ADUserPhoto.ps1
@@ -9,29 +9,100 @@
     
     .PARAMETER Identity
     Specifies an Active Directory user object by providing one of the following property values. The identifier in
-	parentheses is the LDAP display name for the attribute. The acceptable values for this parameter are:
+    parentheses is the LDAP display name for the attribute. The acceptable values for this parameter are:
 
-	-- A Distinguished Name
-	-- A GUID (objectGUID)
-	-- A Security Identifier (objectSid)
-	-- A SAM Account Name (sAMAccountName)
+    -- A Distinguished Name
+    -- A GUID (objectGUID)
+    -- A Security Identifier (objectSid)
+    -- A SAM Account Name (sAMAccountName)
 
-	The cmdlet searches the default naming context or partition to find the object. If two or more objects are
-	found, the cmdlet returns a non-terminating error.
+    The cmdlet searches the default naming context or partition to find the object. If two or more objects are
+    found, the cmdlet returns a non-terminating error.
 
-	This parameter can also get this object through the pipeline or you can set this parameter to an object
-	instance.
+    This parameter can also get this object through the pipeline or you can set this parameter to an object
+    instance.
+    
+    .PARAMETER Path
+    Specifies the path to the location of the new item. Wildcards are not permitted.
+    
+    .PARAMETER Force
+    Allows the function to overwrite existing files. If not set prompt for overwrite will be displayed.
+    Even using the Force parameter, the cmdlet cannot override security restrictions.
+    
     
     .OUTPUTS
     System.Object[]
     
     .EXAMPLE
-    PS > Export-ADUserPhoto user1
-	
+    PS > Export-ADUserPhoto -Identity user1
+    
+    UserSAMAccountName                                                PhotoExported            PhotoINActiveDirectoryExists
+    ------------------                                                -------------            ----------------------------
+    user1                                                                      True                                    True
+    
+    PS > Get-ChildItem -Path .\
+    
+    Directory: C:\Users\Administrator\Desktop
+
+    Mode                LastWriteTime     Length Name
+    ----                -------------     ------ ----
+    -a---          1/9/2016   2:40 PM        910 user1.jpg
+
+    The identity of user from command line, the result file named <SAMAccountName>.jpg saved in the current directory
+    
+    .EXAMPLE
+    
+    PS > "user1","yyyy","xxxx" | Export-ADUserPhoto
+
+    Overwrite File
+    The file C:\Users\Administrator\Desktop\user1.jpg exist. Overwrite?
+    [Y] Yes  [A] All  [N] No  [O] No for All  [C] Cancel  [?] Help (default is "Y"): y
+
+    UserSAMAccountName                                                PhotoExported            PhotoINActiveDirectoryExists
+    ------------------                                                -------------            ----------------------------
+    wojteks                                                                    True                                    True
+    YYYY                                                                       True                                    True
+    XXXX                                                                      False                                   False
+    
+    PS > Get-ChildItem -Path .\
+    
+    Directory: C:\Users\Administrator\Desktop
+
+    Mode                LastWriteTime     Length Name
+    ----                -------------     ------ ----
+    -a---          1/9/2016   2:43 PM        910 user1.jpg
+    -a---          1/9/2016   2:43 PM       1930 yyyy.jpg
+    
+    The identities of users from pipeline as an array of strings, the result files named <SAMAccountName>.jpg saved in the current directory, with the prompt for overwrite.
+    
+    .EXAMPLE
+    
+    PS C:\Users\Administrator\Desktop> Get-ADUser -filter * | Export-ADUserPhoto -Force
+
+    UserSAMAccountName                                 PhotoINActiveDirectoryExists                           PhotoExported
+    ------------------                                 ----------------------------                           -------------
+    Guest                                                                      True                                    True
+    krbtgt                                                                    False                                   False
+    Administrator                                                             False                                   False
+    user1                                                                      True                                    True
+    XXXX                                                                      False                                   False
+    YYYY                                                                       True                                    True
+
+    PS > Get-ChildItem -Path .\
+    
+    Directory: C:\Users\Administrator\Desktop
+
+    Mode                LastWriteTime     Length Name
+    ----                -------------     ------ ----
+    -a---          1/9/2016   2:46 PM        910 user1.jpg
+    -a---          1/9/2016   2:46 PM       1930 yyyy.jpg
+    -a---          1/9/2016   2:46 PM       4720 guest.jpg
+
+    The identities of users from pipeline as Active Directory users objects, the result files named <SAMAccountName>.jpg saved in the current directory, without the prompt for overwrite.
     
     Export the photo stored in the Active Directory for user account to jpg file".
 
-	.LINK
+    .LINK
     https://github.com/it-praktyk/Export-ADUserPhoto
     
     .LINK
@@ -43,13 +114,12 @@
    
     VERSIONS HISTORY
     - 0.1.0 - 2016-01-06 - The first version
-	- 0.2.0 - 2016-01-07 - The function renamed to Export-ADUserPhoto, the parameter UserName renamed to Identity
+    - 0.2.0 - 2016-01-07 - The function renamed to Export-ADUserPhoto, the parameter UserName renamed to Identity
+    - 0.3.0 - 2016-01-09 - The function extended, help updated
 
     TODO
-	- Test parameter Identity from pipeline
-	- Verify declared type of parameter Identity
-	- Update examples
-	        
+    - the situation of write to disk error should be handled
+            
     LICENSE
     Copyright (c) 2016 Wojciech Sciesinski
     This function is licensed under The MIT License (MIT)
@@ -62,31 +132,42 @@
     (including, without limitation, damages for loss of business profits, business interruption, loss of business information,
     or other pecuniary loss) arising out of the use of or inability to use the script or documentation. 
     
-#>  
+#>    
     [CmdletBinding()]
     [OutputType([System.Object[]])]
-    
     Param (
-		[Parameter(Mandatory = $True, ValueFromPipeline = $True)]
-		[alias("UserName")]
-        [String[]]$Identity,
+        [Parameter(Mandatory = $True, ValueFromPipeline = $True)]
+        [alias("UserName")]
+        [Microsoft.ActiveDirectory.Management.ADUser]
+        $Identity,
         [Parameter(Mandatory = $false)]
-        [System.IO.DirectoryInfo]$FolderName
+        [System.IO.DirectoryInfo]$Path,
+        [alias("FolderName")]
+        [Parameter(Mandatory = $false)]
+        [Switch]$Force
     )
     
     Begin {
-	
-		If (!(Get-Module -name 'ActiveDirectory' -ErrorAction SilentlyContinue) ) {
+                
+        If (!(Get-Module -name 'ActiveDirectory' -ErrorAction SilentlyContinue)) {
             
             Import-Module -Name 'ActiveDirectory' -ErrorAction Stop | Out-Null
             
         }
-		
-		if (!$FolderName) {
-		
-			$FolderName = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath((Get-Location).Path)
-			
-		}
+        
+        if (!$FolderName) {
+            
+            $FolderName = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath((Get-Location).Path)
+            
+        }
+        
+        If (!(Test-Path -Path $FolderName -Type Container)) {
+            
+            [String]$ExceptionText = "The folder {0} doesn't exist." -f $FolderName
+            
+            Throw $ExceptionText
+            
+        }
         
         $Results = @()
         
@@ -96,57 +177,184 @@
         
         $Identity | ForEach-Object -Process {
             
+            $WasError = $false
+            
             $CurrentUser = $_
             
-            Try {
-                
-                $user = Get-ADUser $CurrentUser -Properties thumbnailPhoto
-                
-            }
-            Catch {
-                
-                [String]$MessageText = "Active Directory account for the user {0}" -f $CurrentUser
-                
-                Write-Warning -Message $MessageText
-                
-                Continue
-                
-            }
+            $CurrentDomainDN = (Get-ADDomain).DistinguishedName
             
             $Result = New-Object PSObject
             
-            $Result | Add-Member -type NoteProperty -name UserSAMAccountName -value $user.SAMAccountName
-            
             Try {
                 
-                [String]$UserPhotoFullFileName = "{0}\{1}.jpg" -f $FolderName, $CurrentUserName
+                $ADUser = Get-ADUser $CurrentUser -Properties thumbnailPhoto
                 
-                if ($user.thumbnailPhoto) {
+                $Result | Add-Member -type NoteProperty -name UserSAMAccountName -value $ADUser.SAMAccountName
+                
+            }
+            Catch {
+                
+                [String]$MessageText = "Cannot find an object with identity: {0} under: '{1}'." -f $CurrentUser, $CurrentDomainDN
+                
+                Write-Warning -Message $MessageText
+                
+                $WasError = $true
+                
+            }
+            
+            #Perform only if user was found in Active Directory
+            If (!$WasError) {
+                
+                Try {
                     
-                    $user.thumbnailPhoto | Set-Content $UserPhotoFullFileName -Encoding byte
+                    [System.IO.FileInfo]$ADUserPhotoDestinationFile = "{0}\{1}.jpg" -f $FolderName, $ADUser.SAMAccountName
                     
-                    $Result | Add-Member -Type NoteProperty -Name PhotoINActiveDirectoryExists -Value $true
+                    #Block if thumbnailPhoto is not empty
+                    if ($ADUser.thumbnailPhoto) {
+                        
+                        #Block if the file <SAMAccountName>.jpg for current AD user already exist
+                        if (Test-Path -Path $ADUserPhotoDestinationFile -Type Leaf) {
+                            
+                            If ($Force.IsPresent -or $OverwriteAll) {
+                                
+                                $ADUser.thumbnailPhoto | Set-Content -Path $ADUserPhotoDestinationFile.FullName -Encoding byte
+                                
+                                $Result | Add-Member -type NoteProperty -name PhotoExported -value $true
+                                
+                            }
+                            elseif ($PreserveAll) {
+                                
+                                [String]$MessageText = "The photo for the user {0} will not be exported because the file {1} exists." -f $ADUser.SAMAccountName, $ADUserPhotoDestinationFile.FullName
+                                
+                                Write-Warning $MessageText
+                                                                
+                            }
+                            Else {
+                                
+                                #Dialog for decision if Force was not set or Overwrite All not selected previously
+                                [String]$Title = "Overwrite File"
+                                [String]$MessageText = "The file {0} exist. Overwrite?" -f $ADUserPhotoDestinationFile.FullName
+                                
+                                $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+                                                    "Overwrite the existing file."
+                                
+                                $yesall = New-Object System.Management.Automation.Host.ChoiceDescription "&All", `
+                                                     "Overwrite the all existing files."
+                                
+                                $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+                                                     "Retain the existing file."
+                                
+                                $noall = New-Object System.Management.Automation.Host.ChoiceDescription "N&o for All", `
+                                                    "Retain the all existing files."
+                                
+                                $cancel = New-Object System.Management.Automation.Host.ChoiceDescription "&Cancel", `
+                                                     "Cancel."
+                                
+                                $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $yesall, $no, $noall, $cancel)
+                                
+                                $Answer = $host.ui.PromptForChoice($Title, $MessageText, $Options, 0)
+                                
+                                switch ($Answer) {
+                                    
+                                    #Write the file to disk
+                                    0 {
+                                        
+                                        $ADUser.thumbnailPhoto | Set-Content -Path $ADUserPhotoDestinationFile.FullName -Encoding byte
+                                        
+                                        $Result | Add-Member -type NoteProperty -name PhotoExported -value $true
+                                        
+                                    }
+                                    
+                                    #Write the file to disk and overwrite any next file if exist
+                                    1 {
+                                        
+                                        [Bool]$OverWriteAll = $true
+                                        
+                                        $ADUser.thumbnailPhoto | Set-Content -Path $ADUserPhotoDestinationFile.FullName -Encoding byte
+                                        
+                                        $Result | Add-Member -type NoteProperty -name PhotoExported -value $true
+                                        
+                                    }
+                                    
+                                    #No write the current file to disk becaluse "No" was selected
+                                    2 {
+                                        
+                                        [String]$MessageText = "The photo for the user {0} will not be exported because the file {1} exists." -f $ADUser.SAMAccountName, $ADUserPhotoDestinationFile.FullName
+                                        
+                                        Write-Warning $MessageText
+                                        
+                                        $Result | Add-Member -type NoteProperty -name PhotoExported -value $false
+                                        
+                                        Continue
+                                        
+                                    }
+                                    
+                                    #No write file to disk because "No for All" was selected
+                                    3 {
+                                        
+                                        [Bool]$PreserveAll = $true
+                                        
+                                        [String]$MessageText = "The photo for the user {0} will not be exported because the file {1} exists." -f $ADUser.SAMAccountName, $ADUserPhotoDestinationFile.FullName
+                                        
+                                        Write-Warning $MessageText
+                                        
+                                        $Result | Add-Member -type NoteProperty -name PhotoExported -value $false
+                                        
+                                        Continue
+                                        
+                                    }
+                                    #Stop because Cancel was selected
+                                    4 {
+                                        
+                                        Break
+                                        
+                                    }
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        #Block if the file <SAMAccountName>.jpg for current AD user doesn't exist
+                        Else {
+                            
+                            $ADUser.thumbnailPhoto | Set-Content -Path $ADUserPhotoDestinationFile.FullName -Encoding byte
+                            
+                            $Result | Add-Member -type NoteProperty -name PhotoExported -value $true
+                            
+                        }
+                        
+                        $Result | Add-Member -Type NoteProperty -Name PhotoINActiveDirectoryExists -Value $true
+                    }
+                    
+                    #region Block if thumbnailPhoto is empty
+                    Else {
+                        
+                        $Result | Add-Member -Type NoteProperty -Name PhotoINActiveDirectoryExists -Value $false
+                        
+                        $Result | Add-Member -type NoteProperty -name PhotoExported -value $false
+                        
+                    }
+                    
                 }
-                Else {
+                
+                #In case of an error for AD reading e.g. - see TO DO
+                Catch {
                     
                     $Result | Add-Member -Type NoteProperty -Name PhotoINActiveDirectoryExists -Value $false
                     
+                    $Result | Add-Member -type NoteProperty -name PhotoExported -value $false
+                    
+                }
+                
+                #Add the current object to the collection
+                Finally {
+                    
+                    $Results += $Result
+                    
                 }
                 
             }
-            
-            Catch {
-                
-                $Result | Add-Member -Type NoteProperty -Name PhotoINActiveDirectoryExists -Value $false
-                
-            }
-            
-            Finally {
-                
-                $Results += $Result
-                
-            }
-            
         }
         
     }
