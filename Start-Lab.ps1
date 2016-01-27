530a3454fd06f7d378e2bd9826966b25d47a301f
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-01-27 23:14:29 +0100

    0.1 - the first drafts

---
+++ Start-Lab.ps1
@@ -0,0 +1,145 @@
+
+
+Function Start-Lab {
+
+<#
+
+TODO
+Stop Hyper-V host after stopping machines
+Clean code
+Parse CSV input file structure
+#>
+    
+    [CmdletBinding()]
+    param (
+        
+        [parameter(Mandatory = $true)]
+        [String]$LabFile = "C:\Users\Administrator.wojteks\Desktop\LAB.EX2013.VMS.csv",
+        [parameter(Mandatory = $false)]
+        [ValidateSet("Start", "Stop", "Suspend","Resume")]
+        [String]$Operation = "Start",
+        [parameter(Mandatory = $false)]
+        [Int]$CheckStatusEverySeconds = 10
+        
+    )
+	
+	#$StartWaitStates = "
+	
+	Switch ($Operation) {
+	
+		"Start" {}
+		
+		"Stop" {}
+		
+		"Suspend" {}
+		
+		"Resume" {}
+	
+	}
+	
+    
+    If ($Operation -eq "Start") {
+        
+        $VMS = Import-CSV -Path $LabFile | Sort-Object StartOrder
+        
+    }
+    elseif ($Operation -eq "Stop") {
+        
+        $VMS = Import-CSV -Path $LabFile | Sort-Object StartOrder -Descending
+    }
+    
+    
+    $VMS | foreach-Object -Process {
+	
+		$vm = $_
+        
+        If ($Operation -eq "Start") {
+            
+            If ($vm.Start -eq 1) {
+                
+                if ((Get-Vm -Name $vm.Name -ComputerName $vm.ComputerName).State -ne 'Running') {
+                    
+                    Start-VM -Name $vm.Name -ComputerName $vm.ComputerName
+                    
+                    Wait-VM -VMName $vm.Name -HyperVHost $vm.ComputerName -Operation "Start" -CheckStatusEverySeconds $CheckStatusEverySeconds
+                    
+                }
+                Else {
+                    
+                    Write-Verbose "$vm.Name is started now"
+                    
+                }
+                
+            }
+            
+            Else {
+                
+                Write-Verbose "Virtual machine $vm.Name will not be started"
+                
+            }
+        }
+        
+        elseif ($Operation -eq "Stop") {
+            
+            If ($vm.Start -eq 1) {
+                
+                if ((Get-Vm -Name $vm.Name -ComputerName $vm.ComputerName).State -ne 'Off') {
+                    
+                    Stop-VM -Name $vm.Name -ComputerName $vm.ComputerName
+                    
+                    Wait-VM -VMName $vm.Name -HyperVHost $vm.ComputerName -Operation "Stop" -CheckStatusEverySeconds $CheckStatusEverySeconds
+                    
+                }
+                Else {
+                    
+                    Write-Verbose "$vm.Name is started now"
+                    
+                }
+                
+            }
+            
+            Else {
+                
+                Write-Verbose "Virtual machine $vm.Name will not be started"
+                
+            }
+            
+            
+        }
+        
+    }
+    
+}
+
+
+
+    function Wait-VM {
+        
+        param ($VMName,
+            $HyperVHost,
+            $Operation,
+            $CheckStatusEverySeconds)
+        
+        if ($Operation -eq "Start") {
+            
+            while ((((Get-VM -Name $VMName -ComputerName $HyperVHost).HeartBeat).ToString()).Substring(0, 2) -ne 'Ok') {
+                
+                Start-Sleep -Seconds $CheckStatusEverySeconds
+                
+                Write-Verbose "$VMName not ready. Waiting"
+                
+            }
+        }
+        elseif ($Operation -eq "Stop") {
+            
+            while ((Get-VM -Name $VMName -ComputerName $HyperVHost).State -ne 'Off') {
+                
+                Start-Sleep -Seconds 5
+                
+                Write-Verbose "$VMName not stoped yet. Waiting."
+                
+            }
+            
+        }
+        
+    }
